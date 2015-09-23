1682db26a261b5dbd1ac332e393e9e68395da4a2
Alex Blundell <alex@alexblundell.com>
2015-09-23 19:26:22 +0100

    Initial Commit

---
+++ SMSenseMatters/SMSenseMatters.h
@@ -0,0 +1,13 @@
+//
+//  SMSenseMatters.h
+//  SMSenseMatters
+//
+//  Created by Alex Blundell on 23/09/2015.
+//  Copyright © 2015 Alex Blundell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMSenseMatters : NSObject
+
+@end
